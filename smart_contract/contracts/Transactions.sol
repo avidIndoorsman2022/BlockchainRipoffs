3e44977bac83229e0abc403e5aad4c96f7dfce3c
AvidIndoorsman2022 <93170237+avidIndoorsman2022@users.noreply.github.com>
2022-01-11 13:58:01 -0500

    Renamed the overly-aggressive "RipoffReports" to "ReputationReports"
    "BlockchainRipoffs" also became the softer "wallet-reputation".

--- smart_contract/contracts/Transactions.sol
+++ smart_contract/contracts/Transactions.sol
@@ -3,43 +3,43 @@
 pragma solidity ^0.8.0;
 
 contract Transactions {
-    uint public ripoffReportCount;
+    uint public reputationReportCount;
 
-    struct RipoffReportStruct {
+    struct ReputationReportStruct {
         address sender;  
         address receiver;      
-        uint256 timestamp;   // when the ripoff took place
+        uint256 timestamp;   // when the fraud/scam took place
         uint amount;         // units are denoted in 'blockchain' (below)
         uint whoFiled;       // 0=sender, 1=receiver
         bool disputed;       // true if opposing party has lodged a dispute
-        string txnId;        // transaction Id of ripoff
+        string txnId;        // transaction Id of reputation
         string usersMessages; // text of messages left by originator and disputer, if any
         string tokenName;   // unit of amount (above)
     }
 
-    RipoffReportStruct[] public ripoffReports;
+    ReputationReportStruct[] public reputationReports;
 
-    event NewRipoffReport(address sender, address receiver, uint256 timestamp, uint amount, uint whoFiled, bool disputed, string txnId, string messages, string tokenName);
+    event NewReputationReport(address sender, address receiver, uint256 timestamp, uint amount, uint whoFiled, bool disputed, string txnId, string messages, string tokenName);
     
     //
     // This mapping is for quickly updating an existing
     // report.  Given a transaction Id, we can lookup the
-    // index into the RipoffReportStruct to quickly find
-    // the exact RipoffReport.
-    // I.e, txnId => RipoffReportStruct[index]
+    // index into the reputationReportStruct to quickly find
+    // the exact reputationReport.
+    // I.e, txnId => reputationReportStruct[index]
     //
     mapping(string => uint) public transactionIdToIndex; 
 
-    function createRipoffReport(address _sender, 
-                                address _receiver, 
-                                uint256 _timestamp,
-                                uint _amount,        
-                                uint _whoFiled,
-                                bool _disputed,
-                                string memory _txnId,
-                                string memory _usersMessages,
-                                string memory _tokenName) public {
-        ripoffReports.push(RipoffReportStruct(_sender,
+    function createReputationReport(address _sender, 
+                                    address _receiver, 
+                                    uint256 _timestamp,
+                                    uint _amount,        
+                                    uint _whoFiled,
+                                    bool _disputed,
+                                    string memory _txnId,
+                                    string memory _usersMessages,
+                                    string memory _tokenName) public {
+        reputationReports.push(ReputationReportStruct(_sender,
                                               _receiver, 
                                               _timestamp,
                                               _amount, 
@@ -48,40 +48,40 @@ contract Transactions {
                                               _txnId,
                                               _usersMessages, 
                                               _tokenName));
-        ripoffReportCount += 1;
-        transactionIdToIndex[_txnId] = ripoffReportCount;
-        emit NewRipoffReport(_sender, _receiver, _timestamp, _amount, _whoFiled, _disputed, _txnId, _usersMessages, _tokenName);
+        reputationReportCount += 1;
+        transactionIdToIndex[_txnId] = reputationReportCount;
+        emit NewReputationReport(_sender, _receiver, _timestamp, _amount, _whoFiled, _disputed, _txnId, _usersMessages, _tokenName);
     }
 
-    function getAllRipoffReports() public view returns (RipoffReportStruct[] memory) {
-        return ripoffReports;
+    function getAllReputationReports() public view returns (reputationReportStruct[] memory) {
+        return reputationReports;
     }
 
-    function getRipoffReportsCount() public view returns (uint256) {
-        return ripoffReportCount;
+    function getReputationReportsCount() public view returns (uint256) {
+        return reputationReportCount;
     }
 
     //
     // Allows originators to update the report, either cancelling it or
     // adding to the messages.  Also allows opponents to dispute.
     //
-    function updateRipoffReport(string memory _txnId, 
-                                bool _disputed,
-                                string memory _messages) payable public returns (string memory _returnCode) {
+    function updateReputationReport(string memory _txnId, 
+                                    bool _disputed,
+                                    string memory _messages) payable public returns (string memory _returnCode) {
         uint returnCode; // 0 = success, anything else is an error
         uint index = transactionIdToIndex[_txnId];
-        RipoffReportStruct memory ripoffReport = ripoffReports[index];
-        if (strcmp(_txnId, ripoffReport.txnId)) {
+        ReputationReportStruct memory reputationReport = reputationReports[index];
+        if (strcmp(_txnId, reputationReport.txnId)) {
           //
           // In order to maintain integrity, the sender MUST
-          // be either the ripoffReports .sender or .receiver.
+          // be either the reputationReports .sender or .receiver.
           // It's a payable function since we are updating data
           // on chain.
           // 
-          if (msg.sender == ripoffReport.sender || 
-              msg.sender == ripoffReport.receiver) {
-              ripoffReport.disputed = _disputed;
-              ripoffReport.usersMessages = _messages;
+          if (msg.sender == reputationReport.sender || 
+              msg.sender == reputationReport.receiver) {
+              reputationReport.disputed = _disputed;
+              reputationReport.usersMessages = _messages;
               returnCode = 0;
           } else {
             // Send back failure - update can only be done by the sender or receiver of the original transaction
