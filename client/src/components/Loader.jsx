bdca104b0cdb8e11dec5125bfe7da91842b5d111
AvidIndoorsman2022 <avidIndoorsman2021@gmail.com>
2022-01-21 12:01:57 -0500

    Added spinner to Loader.

--- client/src/components/Loader.jsx
+++ client/src/components/Loader.jsx
@@ -1,9 +1,7 @@
-import React, { Component } from "react";
-
-export class Loader extends Component {
-  render() {
-    return <div>Loader</div>;
-  }
-}
+const Loader = () => (
+  <div className="flex justify-center items-center py-3">
+    <div className="animate-spin rounded-full h-16 w-16 border-b-2 border-blue-700" />
+  </div>
+);
 
 export default Loader;
