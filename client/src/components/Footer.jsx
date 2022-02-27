2d12d7dfa13d27e0b960c3d7badcef753ac94931
AvidIndoorsman2022 <avidIndoorsman2021@gmail.com>
2022-02-27 16:18:13 -0500

    Minor UI changes.

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -2,7 +2,13 @@ import React, { Component } from "react";
 
 export class Footer extends Component {
   render() {
-    return <div>Footer</div>;
+    return (
+      <div>
+        <p className="text-left mt-5 text-white font-light w-full text-sm">
+          Powered by Etherscan.io APIs
+        </p>
+      </div>
+    );
   }
 }
 
